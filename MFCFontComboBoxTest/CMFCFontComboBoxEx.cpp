72889478db7e1623d7dae50f06bfd4a822d54e5a
Ladislav Dudas <ladislav_dudas@foxitsoftware.com>
2023-01-28 20:19:53 +0100

    MFCFontComboBoxTest: Simplify OnDrawItem

--- MFCFontComboBoxTest/CMFCFontComboBoxEx.cpp
+++ MFCFontComboBoxTest/CMFCFontComboBoxEx.cpp
@@ -33,22 +33,25 @@ END_MESSAGE_MAP()
 void CMFCFontComboBoxEx::OnDrawItem(int nIDCtl, LPDRAWITEMSTRUCT lpDrawItemStruct)
 {
     if (nIDCtl == this->GetDlgCtrlID()) {
-        if (lpDrawItemStruct->itemID == 0 || lpDrawItemStruct->itemID == 1) {
-            LPCTSTR recent_fonts_text = _T("Recent fonts");
-            LPCTSTR all_fonts_text = _T("All fonts");
-            LPCTSTR text_to_draw = lpDrawItemStruct->itemID == 0 ? recent_fonts_text : all_fonts_text;
-            int text_to_draw_len = static_cast<int>(_tcslen(text_to_draw));
+        if (this->GetItemDataPtr(lpDrawItemStruct->itemID) == nullptr) {
+            // Drawing "Recent fonts" or "All fonts"
+            CString item_text;
+            this->GetLBText(lpDrawItemStruct->itemID, item_text);
             HBRUSH brush = ::CreateSolidBrush(RGB(200, 200, 200));
             HFONT font = NULL;
+            HGDIOBJ old_font = NULL;
             LOGFONT log_font = {};
             if (this->GetFont()->GetLogFont(&log_font) != 0) {
                 log_font.lfWeight = FW_BOLD;
                 font = ::CreateFontIndirect(&log_font);
+                old_font = ::SelectObject(lpDrawItemStruct->hDC, font);
             }
+
             ::FillRect(lpDrawItemStruct->hDC, &lpDrawItemStruct->rcItem, brush);
-            HGDIOBJ old_font = ::SelectObject(lpDrawItemStruct->hDC, font);
-            ::DrawText(lpDrawItemStruct->hDC, text_to_draw, text_to_draw_len, &lpDrawItemStruct->rcItem, DT_LEFT | DT_TOP);
-            ::SelectObject(lpDrawItemStruct->hDC, old_font);
+            ::DrawText(lpDrawItemStruct->hDC, item_text, item_text.GetLength(), &lpDrawItemStruct->rcItem, DT_LEFT | DT_TOP);
+
+            if (old_font)
+                ::SelectObject(lpDrawItemStruct->hDC, old_font);
             ::DeleteObject(font);
             ::DeleteObject(brush);
             return;
